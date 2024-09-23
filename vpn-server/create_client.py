559eb6ace743694d0cf49f8ae5c8b40e8ab6bb94
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-09-23 21:32:20 +0700

    added folder for all the code for launch new vpn nodes

---
+++ vpn-server/create_client.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+import subprocess
+import json
+
+
+name = "test"#input("Name: ")
+enddate = "2024_10_23"#input("End date: YYYY-MM-DD): ")
+
+
+def create_configs(name: str, enddate: str):
+    with open('config.json', 'r') as file:
+        data = json.load(file)
+        host = data.get('hostname')
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    client_name_1 = f"{host}_{name}_1_{current_date}_{enddate}"
+    client_name_2 = f"{host}_{name}_2_{current_date}_{enddate}"
+
+    print(f"Created names are:")
+    print(client_name_1)
+    print(client_name_2)
+
+    os.environ['NEW_OPENVPN_CLIENT'] = client_name_1
+    subprocess.run(['./install.sh'])
+
+    os.environ['NEW_OPENVPN_CLIENT'] = client_name_2
+    subprocess.run(['./install.sh'])
+
+
+create_configs(name, enddate)
+
+print(f"Successfully created configs")
