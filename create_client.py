d3533927f3238be056ea2c6bdd7acec2d89deda6
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-09-23 21:11:54 +0700

    upd hostname and py create confs

--- create_client.py
+++ create_client.py
@@ -1,24 +1,33 @@
 import os
 import datetime
 import subprocess
+import json
 
 
 name = "test"#input("Name: ")
-specified_date = "2024_10_23"#input("End date: YYYY-MM-DD): ")
-host = "MEL_RIG_4"
-current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
-client_name_1 = f"{host}_{name}_1_{current_date}_{specified_date}"
-client_name_2 = f"{host}_{name}_2_{current_date}_{specified_date}"
-
-print(f"Created names are:")
-print(client_name_1)
-print(client_name_2)
+enddate = "2024_10_23"#input("End date: YYYY-MM-DD): ")
 
 
-def run_openvpn_script(client_name):
-    os.environ['NEW_OPENVPN_CLIENT'] = client_name
+def create_configs(name: str, enddate: str):
+    with open('config.json', 'r') as file:
+        data = json.load(file)
+        host = data.get('hostname')
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    client_name_1 = f"{host}_{name}_1_{current_date}_{enddate}"
+    client_name_2 = f"{host}_{name}_2_{current_date}_{enddate}"
+
+    print(f"Created names are:")
+    print(client_name_1)
+    print(client_name_2)
+
+    os.environ['NEW_OPENVPN_CLIENT'] = client_name_1
     subprocess.run(['./install.sh'])
-exit()
-run_openvpn_script(client_name_1)
-run_openvpn_script(client_name_2)
+
+    os.environ['NEW_OPENVPN_CLIENT'] = client_name_2
+    subprocess.run(['./install.sh'])
+
+
+create_configs(name, enddate)
+
+print(f"Successfully created configs")
