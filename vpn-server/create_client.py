27ffe74e64015faee1fc5c56b6c905acaaadcd08
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-09-25 19:38:13 +0700

    upd

--- vpn-server/create_client.py
+++ vpn-server/create_client.py
@@ -11,13 +11,32 @@ def new_client(name='DEFAULT', id=1, enddate='2099-12-28'):
     client_name = f"{name}_{host}_{current_date}_{enddate}_{id}"
     print(f"Creating config {client_name}")
 
-    file = open('client_name.txt', 'w')
-    file.write(client_name + '\n')
+    try:
+        with open('client_name.txt', 'w') as file:
+            file.write(client_name + '\n')
 
-    subprocess.run(['sudo', 'chmod', '+x', './openvpn_install.sh'])
-    subprocess.run(['sudo', './openvpn_install.sh'])
+        script_path = os.path.join(os.getcwd(), 'openvpn-install.sh')
+
+        # Установите права на выполнение
+        result = subprocess.run(
+            ['sudo', 'chmod', '+x', script_path], check=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            raise Exception("Failed to change permissions for the script.")
+
+        result = subprocess.run(['bash', script_path],
+                                check=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            raise Exception(f"Failed to execute the script. Error: {
+                            result.stderr.strip()}")
+
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
 
 
-new_client()
-
-print(f"Successfully created configs")
+if __name__ == "__main__":
+    try:
+        new_client()
+        print("Successfully created configs")
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
