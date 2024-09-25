027608fa8031c4e6f17683ce2f96f615b846e4cb
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-09-25 19:43:43 +0700

    upd

--- vpn-server/openvpn-install.sh
+++ vpn-server/openvpn-install.sh
@@ -197,7 +197,7 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	done
 	echo
 	echo "Enter a name for the first client:"
-	client=`cat client_name.txt`
+	unsanitized_client=`cat client_name.txt`
 	# Allow a limited set of characters to avoid conflicts
 	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
 	[[ -z "$client" ]] && client="client"
