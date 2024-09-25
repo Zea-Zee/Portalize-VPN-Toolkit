30f2b9bb722db19b2a45151c833f7bbc62cc228d
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-09-25 18:05:54 +0700

    openvpn_install.sh upd

--- vpn-server/openvpn_install.sh
+++ vpn-server/openvpn_install.sh
@@ -75,25 +75,29 @@ TUN needs to be enabled before running this installer."
 fi
 
 new_client () {
-	# Generates the custom client.ovpn
-	client="$1"
-	
-	{
-	cat /etc/openvpn/server/client-common.txt
-	echo "<ca>"
-	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
-	echo "</ca>"
-	echo "<cert>"
-	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
-	echo "</cert>"
-	echo "<key>"
-	cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
-	echo "</key>"
-	echo "<tls-crypt>"
-	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
-	echo "</tls-crypt>"
-	} > ~/"$client".ovpn
+    # Принимаем имя клиента как аргумент функции
+    client="$1"
+
+    # Генерируем кастомный файл client.ovpn
+    {
+        cat /etc/openvpn/server/client-common.txt
+        echo "<ca>"
+        cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+        echo "</ca>"
+        echo "<cert>"
+        sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
+        echo "</cert>"
+        echo "<key>"
+        cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
+        echo "</key>"
+        echo "<tls-crypt>"
+        sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+        echo "</tls-crypt>"
+    } > ~/"$client".ovpn
+
+    echo "Файл конфигурации для клиента $client создан."
 }
+
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	# Detect some Debian minimal setups where neither wget nor curl are installed
